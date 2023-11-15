640156d737d7ceb39929e146d7580ae6a6f5d042
Denvi337 <danil.kryt.run@gmail.com>
2023-11-15 15:59:33 +0500

    Fix chart bug
    Fixed a chart bug so it would display, changed style to show chart properly

--- chart.js
+++ chart.js
@@ -1,35 +1,45 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let labels = ["AUG'21", "SEP", "OCT", "NOV"];
+google.charts.load("current", {packages:['corechart']});
+    google.charts.setOnLoadCallback(drawChart);
 
-let itemData = [63000, 80000, 97000, 140000];
+      function drawChart() {
+        let profitVar2 = parseInt(document.querySelector('#amount').value);
+        let other = 100000 - profitVar2;
 
-const data = {
-    labels: labels,
-    datasets: [{
-        data: itemData,
-        backgroundColor: ['rgb(66, 221, 245)', 'rgb(87, 37, 41)']
-    }]
-};
+        let data = google.visualization.arrayToDataTable([
+            ['', 'AI Rnd', 'AI Rtx', 'AI Rnx', { role: 'annotation' } ],
+            ["JUN", 80000, 60000, 40000, ''],
+            ["JUL", 90000, 65000, 45000, ''],
+            ["AUG", 80000, 55000, 35000, ''],
+            ["SEP", 90000, 60000, 40000, ''],
+            ["OCT", 110000, 70000, 50000, ''],
+            ["NOV", 120000, 80000, 55000, ''],
+            ["DEC", 140000, 90000, 60000, ''],
+            ["JAN'23", 135000, 85000, 65000, ''],
+          ]);
 
-const config = {
-    type: 'bar',
-    data: data,
-    options: {
-        plugins: {
-            legend: {
-                display: false
+        let view = new google.visualization.DataView(data);
+        view.setColumns([0, 1, 2, 
+                        {calc: "stringify",
+                         type: "string",
+                         role: "annotation" },
+                        3]);
+
+        let options = {
+            width: 860,
+            height: 400,
+            legend: { position: 'top', maxLines: 3 },
+            bar: { groupWidth: '75%' },
+            isStacked: false,
+            colors: ['#03cf75', '#426b2d', '#e0ad26'],
+            dataTextStyle: { color: '#fcfcfc' },
+            legendTextStyle: { color: '#fcfcfc' },
+            titleTextStyle: { color: '#fcfcfc' },
+            hAxis: {
+                textStyle:{color: '#fcfcfc'}
             },
-            title: {
-                display: true,
-                text: 'Test'
-            }
-        }
-    }
-};
+            backgroundColor: "transparent"
+          };
 
-const chart = new Chart(
-    document.getElementById('myChart'),
-    config
-);
-    
-  });
+        let chart = new google.visualization.ColumnChart(document.getElementById("columnchart_values"));
+        chart.draw(view, options);
+      }
