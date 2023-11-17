3d1632a767e47768f727c7296b362099f45df398
Denvi337 <danil.kryt.run@gmail.com>
2023-11-17 13:05:01 +0500

    Change chart, change menu style to fit
    Changed chart to better fit the design, improved it, changed style of menu to show up properly

--- chart.js
+++ chart.js
@@ -1,45 +1,26 @@
-google.charts.load("current", {packages:['corechart']});
-    google.charts.setOnLoadCallback(drawChart);
+google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
 
       function drawChart() {
-        let profitVar2 = parseInt(document.querySelector('#amount').value);
-        let other = 100000 - profitVar2;
+        var data = google.visualization.arrayToDataTable([
+          ['Date',  'AI Rnd', 'Ai Rtx'],
+          ['AUG (21)', 100000,         63000]
+        ]);
 
-        let data = google.visualization.arrayToDataTable([
-            ['', 'AI Rnd', 'AI Rtx', 'AI Rnx', { role: 'annotation' } ],
-            ["JUN", 80000, 60000, 40000, ''],
-            ["JUL", 90000, 65000, 45000, ''],
-            ["AUG", 80000, 55000, 35000, ''],
-            ["SEP", 90000, 60000, 40000, ''],
-            ["OCT", 110000, 70000, 50000, ''],
-            ["NOV", 120000, 80000, 55000, ''],
-            ["DEC", 140000, 90000, 60000, ''],
-            ["JAN'23", 135000, 85000, 65000, ''],
-          ]);
+        var options_stacked = {
+          isStacked: false,
+          height: 300,
+          legend: {position: 'top', maxLines: 3},
+          vAxis: {minValue: 0},
+          backgroundColor: "transparent",
+          legendTextStyle: { color: '#FFF' },
+          titleTextStyle: { color: '#FFF' },
+          hAxis: {
+            textStyle:{color: '#FFF'}
+        }
+        };
 
-        let view = new google.visualization.DataView(data);
-        view.setColumns([0, 1, 2, 
-                        {calc: "stringify",
-                         type: "string",
-                         role: "annotation" },
-                        3]);
+        var chart = new google.visualization.SteppedAreaChart(document.getElementById('chart_div'));
 
-        let options = {
-            width: 860,
-            height: 400,
-            legend: { position: 'top', maxLines: 3 },
-            bar: { groupWidth: '75%' },
-            isStacked: false,
-            colors: ['#03cf75', '#426b2d', '#e0ad26'],
-            dataTextStyle: { color: '#fcfcfc' },
-            legendTextStyle: { color: '#fcfcfc' },
-            titleTextStyle: { color: '#fcfcfc' },
-            hAxis: {
-                textStyle:{color: '#fcfcfc'}
-            },
-            backgroundColor: "transparent"
-          };
-
-        let chart = new google.visualization.ColumnChart(document.getElementById("columnchart_values"));
-        chart.draw(view, options);
+        chart.draw(data, options_stacked);
       }
