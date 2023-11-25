60e217c85a9d72072bb5824a989177b967f3de69
Denvi337 <danil.kryt.run@gmail.com>
2023-11-25 13:29:24 +0500

    Update menu style
    Updated the style of range sliders

---
+++ landing-page.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const sliders = document.querySelectorAll('.range-slider');
+    const outputs = document.querySelectorAll('output');
+  
+    sliders.forEach((slider, index) => {
+      slider.addEventListener('input', function() {
+        outputs[index].textContent = slider.value;
+      });
+    });
+  });
+  
+document.addEventListener('DOMContentLoaded', function() {
+  const slider = document.getElementById('amount');
+  
+  slider.addEventListener('input', function() {
+    const percent = (100 * (slider.value - slider.min)) / (slider.max - slider.min);
+    slider.style.background = `linear-gradient(to right, #61c499 0%, #01b869 ${percent}%, #d3d3d3 ${percent}%, #d3d3d3 100%)`;
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const slider = document.getElementById('length');
+    
+    slider.addEventListener('input', function() {
+      const percent = (100 * (slider.value - slider.min)) / (slider.max - slider.min);
+      slider.style.background = `linear-gradient(to right, #ffbf00 0%, #01b869 ${percent}%, #d3d3d3 ${percent}%, #d3d3d3 100%)`;
+    });
+  });
+
+
